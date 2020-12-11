ea07f8d3e2cb6b15939f2590642eb70b7c26e4ba
ShowlandProductions <35757926+ShowlandProductions@users.noreply.github.com>
2020-12-10 19:32:48 -0800

    Add files via upload
    Changed my js title to script

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("moment: ", moment());
+var today = moment().format("dddd");
+console.log("today: ", today)
+
+
+//$(document).ready(function() {
+    //I'm using Moment to display the time at the top of the page
+    //Information for the date and time on the top of the page. The page needs to update when refreshed
+    //var dateTimeDiv = $("<div>")
+    //var currentDateTime = moment().format('MMMM Do YYYY')
+    
+    //dateTimeDiv.html(currentDateTime);
+    //$("#currentDay").append(DateTime);
+  
+  //created local storage setting for scheduler 
+
+  
